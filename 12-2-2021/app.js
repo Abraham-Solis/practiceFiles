b99ff908769414673b10700292e50c62461c5475
email <abe.solis2015@gmail.com>
2021-12-02 14:11:38 -0800

    code 12-2-2021

---
+++ 12-2-2021/app.js
@@ -0,0 +1,15 @@
+// const calculator = require('./calculator.js')
+
+// const operation = process.argv[2]
+// const a = process.argv[3]
+// const b = process.argv[4]
+
+// calculator(operation, a, b)
+
+const movie = require(`./movie.js`)
+
+const display = arr =>{
+  arr.forEach(elem => console.log(elem))
+}
+
+display(movie)
---
+++ 12-2-2021/movie.js
@@ -0,0 +1,6 @@
+const movie = ["House of Gucci" , "Resident Evil: Welocme to Racoon City" , "Ghostbusters: Afterlife" , "Encanto" , "Eternals"
+  ]
+
+console.log(movie)
+
+module.exports = movie
