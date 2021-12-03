f7304f154f734602bb17fd51480bc39012701187
email <abe.solis2015@gmail.com>
2021-12-03 14:40:48 -0800

    code

---
+++ day2node/movie.js
@@ -0,0 +1,20 @@
+
+const axios = require('axios');
+
+let title = process.argv[2]
+
+
+
+// Make a request for a user with a given ID
+axios.get(`http://www.omdbapi.com/?apikey=trilogy&t=${title}`)
+  .then(function (response) {
+    // handle success
+    console.log(`Movie: ${response.data.Title}`);
+    console.log(`Year: ${response.data.Year}`);
+    console.log(`Rating: ${response.data.Rated}`);
+    console.log(`Director:${response.data.Director}`);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });
