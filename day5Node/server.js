5f023bdc74c4c36679725a13e53f99014201255d
email <abe.solis2015@gmail.com>
2021-12-09 10:56:04 -0800

    local hosting with html

--- day5Node/server.js
+++ day5Node/server.js
@@ -1,17 +1,22 @@
-const express = require('express')
-
-const app = express()
+const express = require('express') //calling to express
+const path = require('path') //call back
+const app = express() //express fuction
 
 app.get('/movies',(req, res) =>{
-  res.send(`Shrek, Shrek 2, Shrek 3`)
+  res.sendFile(path.join (__dirname, 'movies.html'))
 })
 
 app.get('/games', (req, res) => {
-  res.send(`Battle Field, Rocket League, League of Legends`)
+  res.sendFile(path.join(__dirname,  'games.html'))
 })
 
 app.get('/music', (req, res) => {
-  res.send(`Lofi, Rock, Hip-Hop`)
+  res.sendFile(path.join(__dirname, 'Music.html'))
+})
+
+
+app.get('/food', (req, res) => {
+  res.sendFile(path.join(__dirname, 'food.html'))
 })
 
 
